44f7ffa37f800acf79e5344fe84fe4b697b4e10a
Spencer Moody <spencer.moody@outlook.com>
2020-03-30 14:48:57 -0700

    begin debugging process of js
    coauthored by: Taylor Phillips <taylorphillips133@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
+    event.preventDefault();
     var name = $("#name").val();
-    var id = ["flavors", "scoops", "bowls"];
+    var choice = ["flavors", "scoops", "bowls"];
     var order = [];
 
+    choices.forEach(function(choice) {
+      var userInput = $("select#"+choice).val();
+      $("." + choice).text(userInput);
+      
+    });
+
+    yourOrder.forEach(function(item) {
+      $("#order").append("<li>"+item+"</li>");
+    })
+  
     $(".name").text(name);
-  }
-}
+    $("#order").show();
+  });
+});
