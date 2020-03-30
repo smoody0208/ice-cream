cd63ecc28ce52b07ab247bbda1089e10097ce34e
Spencer Moody <spencer.moody@outlook.com>
2020-03-30 14:18:44 -0700

    add document ready function and name our variables in js
    coauthored by Taylor Phillips <taylorphillips133@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    var name = $("#name").val();
+    var id = ["flavors", "scoops", "bowls"];
+    var order = [];
+
+    $(".name").text(name);
+  }
+}
